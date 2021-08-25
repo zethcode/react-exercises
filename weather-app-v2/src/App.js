3e5bedef224665b656ed2bf8164b73dea02821d6
zethcode <89132475+zethcode@users.noreply.github.com>
2021-08-26 00:40:20 +0800

    Updates added enough for a functioning weather app (API keys are missing)

--- weather-app-v2/src/App.js
+++ weather-app-v2/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [weather, setWeather] = useState({})
+  const [coordinates, setCoordinates] = useState([])
+  const [locations, setLocations] = useState('cebu')
+  const [photos, setPhotos] = useState([])
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setCoordinates({lat: position.coords.latitude, long: position.coords.longitude});
+    })
+  }
+
+  function ifClicked() {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${locations}&APPID={API_KEY_FOR_WEATHER_API}&units=metric`
+    )
+      .then((res) => {
+        if (res.ok) {
+          console.log(res.status);
+          return res.json();
+        } else {
+          if (res.status === 404) {
+            return alert("Oops, there seems to be an error!(wrong location)");
+          }
+          alert("Oops, there seems to be an error!");
+          throw new Error("You have an error");
+        }
+      })
+      .then((object) => {
+        setWeather(object);
+        console.log(weather);
+      })
+      .catch((error) => console.log(error));
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${locations}&client_id={API_KEY_FOR_UNSPLASH}`
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("You made a mistake");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setPhotos(data?.results[0]?.urls?.raw);
+      })
+      .catch((error) => console.log(error));
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <div className="search">
+          <input
+            type="text"
+            value={locations}
+            onChange={(e) => setLocations(e.target.value)}
+            placeholder="Enter location"
+            className="location_input"
+          />
+          <button className="location_searcher" onClick={ifClicked}>
+            Search Location
+          </button>
+        </div>
+        <div className="app__data">
+          <p className="temp">Current Temparature: {weather?.main?.temp}</p>
+        </div>
+        <img className="app__image" src={photos} alt="" />
+      </div>
     </div>
   );
 }
